b4ec61b5e91d803af7b7f397e545026e6235adb1
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-13 10:53:29 +0200

    Update practice

--- recursion.rb
+++ recursion.rb
@@ -14,8 +14,8 @@ puts "Heaviest rock is: #{rocks.inject { |max_rock, rock| max_rock > rock ? max_
 
 def rock_judger(rocks_arr)
   if rocks_arr.length <= 2 # the base case
-    a = rocks_arr[0]
-    b = rocks_arr[-1]
+    a = rocks_arr.first
+    b = rocks_arr.last
   else
     a = rock_judger(rocks_arr.slice!(0, rocks_arr.length / 2))
     b = rock_judger(rocks_arr)
