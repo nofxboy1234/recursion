e5ed5068a4869f7c553bdb1efd390ee456a8862f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-13 13:43:54 +0200

    Update practice

--- recursion2.rb
+++ recursion2.rb
@@ -32,3 +32,24 @@ def sum_range_recursive(n)
   sum_range_recursive(n - 1) + n
 end
 puts sum_range(3)
+
+# call sum_range_recursive(3)
+# n is 3
+# n not less than or equal to 0, no return
+
+# call sum_range_recursive(2)
+# n is 2
+# n not less than or equal to 0, no return
+
+# call sum_range_recursive(1)
+# n is 1
+# n not less than or equal to 0, no return
+
+# call sum_range_recursive(0)
+# n is 0
+# n is less than or equal to 0, return 0
+
+# sum_range_recursive(1) => (0) + 1
+# sum_range_recursive(2) => (1) + 2
+# sum_range_recursive(3) => (3) + 3
+#                        => 6
