df511532724f45d3c59501e3d37124d357fa8915
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-13 15:05:29 +0200

    Update practice

--- recursion2.rb
+++ recursion2.rb
@@ -1,37 +1,37 @@
-def count_down(n)
-  for i in (1..n).to_a.reverse do
-    puts i
-  end
-  puts 'Hooray'
-end
-count_down(3)
+# def count_down(n)
+#   for i in (1..n).to_a.reverse do
+#     puts i
+#   end
+#   puts 'Hooray'
+# end
+# count_down(3)
 
-def count_down_recursive(n)
-  if n <= 0
-    puts 'Hooray'
-    return
-  end
+# def count_down_recursive(n)
+#   if n <= 0
+#     puts 'Hooray'
+#     return
+#   end
 
-  puts n
-  count_down_recursive(n - 1)
-end
-count_down(3)
+#   puts n
+#   count_down_recursive(n - 1)
+# end
+# count_down(3)
 
-def sum_range(n)
-  total = 0
-  for i in (1..n).to_a.reverse do
-    total += i
-  end
-  total
-end
-puts sum_range(3)
+# def sum_range(n)
+#   total = 0
+#   for i in (1..n).to_a.reverse do
+#     total += i
+#   end
+#   total
+# end
+# puts sum_range(3)
 
-def sum_range_recursive(n)
-  return 0 if n <= 0
+# def sum_range_recursive(n)
+#   return 0 if n <= 0
 
-  sum_range_recursive(n - 1) + n
-end
-puts sum_range(3)
+#   sum_range_recursive(n - 1) + n
+# end
+# puts sum_range(3)
 
 # call sum_range_recursive(3)
 # n is 3
@@ -79,7 +79,7 @@ def get_all_children_names(person)
   end
   child_names = person[:children].map { |p| p[:name] }
 
-  return child_names.concat(nested_child_names)
+  child_names.concat(nested_child_names)
 end
 
 # p [[1, 2], 3].flatten
