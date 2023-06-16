897fc3bdd7b6fefeb93f4425c99f93c9d035c350
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-16 11:30:17 +0200

    Update practice

--- recursion2.rb
+++ recursion2.rb
@@ -304,14 +304,14 @@ def fib(num, tabs = 0)
     1
   else
     two_before_num = fib(num - 2, tabs + 2)
-    puts "#{tabs.zero? ? '|' : '-' * tabs}"
+    puts "#{tabs.zero? ? '|' : '-' * tabs}two_before_num: #{two_before_num}"
     one_before_num = fib(num - 1, tabs + 2)
-    puts "#{tabs.zero? ? '|' : '-' * tabs}"
+    puts "#{tabs.zero? ? '|' : '-' * tabs}one_before_num: #{one_before_num}"
     puts "#{tabs.zero? ? '|' : '-' * tabs}#{[two_before_num, one_before_num]}"
   
     two_before_num + one_before_num
   end
 end
 
-binding.pry
+# binding.pry
 p fib(3)
